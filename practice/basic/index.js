4840ec03476b17024f2320e934faf781c43f567a
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-06-07 06:13:39 -0300

    add: basic example

---
+++ practice/basic/index.js
@@ -0,0 +1,14 @@
+require('./src/index.es6.js');
+require('./src/index.coffee');
+require('./src/index.css');
+require('./src/index.scss');
+require('./src/index.less');
+require('./src/index.html');
+require('lodash');
+
+var imgBig = '<img src="' + require("./src/image_big.jpg") + '" />';
+var imgSmall = '<img src="' + require("./src/image_small.png") + '" />';
+
+module.exports = function() {
+  return null;
+};
---
+++ practice/basic/webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack'),
+  path = require('path');
+
+module.exports = {
+  debug: true,
+  entry: {
+    main: './index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [{
+      test: /\.es6.js$/,
+      loader: "babel-loader"
+    }, {
+      test: /\.coffee$/,
+      loader: "coffee-loader"
+    }, {
+      test: /\.css$/,
+      loader: "css-loader!autoprefixer-loader"
+    }, {
+      test: /\.scss$/,
+      loader: "css-loader!sass-loader"
+    }, {
+      test: /\.less$/,
+      loader: "css-loader!less-loader"
+    }, {
+      test: /\.(png|jpg|gif)$/,
+      loader: "url-loader?limit=5000&name=img/img-[hash:6].[ext]"
+    }, {
+      test: /\.html$/,
+      loader: "html-loader"
+    }, {
+      test: require.resolve("lodash"),
+      loader: 'expose?_'
+    }]
+  }
+};
